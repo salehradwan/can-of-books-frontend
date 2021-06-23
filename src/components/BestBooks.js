935e00811446adf935794d83381768e08560ff57
salehradwan <saleh-radwan@outlook.com>
2021-06-23 11:50:47 +0300

    update the get book

--- src/components/BestBooks.js
+++ src/components/BestBooks.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import axios from 'axios';
+import { withAuth0 } from '@auth0/auth0-react';
 
 class BestBooks extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            serverUrl: process.env.REACT_APP_PORT,
+            userEmail: this.props.auth0.user.email,
+            booksData: []
+        }
+    }
+
+    componentDidMount = () => {
+        axios.get(`${this.state.serverUrl}/books?email=${this.state.userEmail}`).then(response => {
+            this.setState({
+                booksData: response.data.books
+            })
+        }).catch(
+            error => {
+                alert(error.message);
+            }
+        );
+    }
     render() {
         return (
             <>
                 <h2>My Books</h2>
-                {this.props.books.length && this.props.books.map((book, idx) => (
+                {this.state.booksData.length > 0 && this.state.booksData.map((book, idx) => (
                     
                     <>
                         <div key={idx}>
@@ -24,4 +46,4 @@ class BestBooks extends React.Component {
     }
 }
 
-export default BestBooks;
+export default withAuth0(BestBooks);
